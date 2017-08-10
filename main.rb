9a1934f5df427adef03ee9a15acf8ee88ffa3662
Matt Reisen <matthewdreisen@hotmail.com>
2017-08-10 16:50:13 -0500

    Fresh start

--- main.rb
+++ main.rb
@@ -1,118 +1,66 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require 'csv'
+require "sinatra"
+require "sinatra/reloader"
+require "csv"
 
-configure do
-  enable :sessions
-  set :session_secret, "secret"
+get "/" do 
+ erb :index
 end
 
-get '/' do
-  erb :index, :locals => {accounts: getAccountList}
+get "/fullrpt" do
+	erb :fullrpt
 end
 
-get '/full' do
-  erb :full, :locals => {accounts: getAccountList}
+get "/account" do
+	erb :account
 end
 
-get '/account' do
-  erb :account, :locals => {account: selectAccount(params['name'].to_sym)}
-end
-
-get '/login' do
-  erb :login
-end
-
-post '/login' do
-  if checkLoginInfo
-    logIn(params['username'],params['password'])
-    redirect to('/admin')
-  end
-  redirect to('/login')
-end
-
-get '/admin' do
-  erb :admin
-end
-
-# ------------------------------------------------------------------------
-
-class Account
-  attr_accessor :name
-  attr_accessor :transactions
-
-  def initialize(name, transactions)
-    @name = name
-    @transactions = transactions
-  end
-
-  def category_total(category)
-    @transactions[category].sum.round(2)
-  end
-
-  def category_average(category)
-    (self.category_total(category) / @transactions[category].length).round(2)
-  end
-
-  def balance
-    balance = 0
-    @transactions.each_key { |category| balance += category_total(category) }
-    balance.round(2)
-  end
-end
-
-# ------------------------------------------------------------------------
 
 helpers do
-  def csvData
-    data = {}
-    CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
-      data[row["Account"].chomp.to_sym] = {}
-    end
-    CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
-      data[row["Account"].chomp.to_sym][row["Category"].chomp.to_sym] = []
-    end
-    CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
-      account = row["Account"].chomp.to_sym
-      category = row["Category"].chomp.to_sym
-      outflow = -row["Outflow"][1..-1].to_f
-      inflow = row["Inflow"][1..-1].to_f
-      data[account][category] << outflow + inflow
-    end
-    data
+accounts = {}
+
+
+CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
+
+  # Add a key for each account to the accounts Hash.
+  account = row["Account"].chomp
+
+  if !accounts[account]
+    accounts[account] = {
+      tally: 0.00,
+      categories: {
+      }
+    }
   end
 
-  def getAccountList
-    data = csvData; accounts = []
-    data.each_key { |name| accounts << Account.new(name, data[name])}
-    accounts
+  # Set the account which is being affected by this iteration.
+  current_account = accounts[account]
+
+  # Clean up outflow and inflow.
+  outflow = row["Outflow"].gsub(/[,\$]/, "").to_f.round(2)
+  inflow = row["Inflow"].gsub(/[,\$]/, "").to_f.round(2)
+  transaction_amount = inflow - outflow
+
+  # Keep a tally for current balance of the account.
+  current_account[:tally] += transaction_amount
+
+  category = row["Category"].chomp
+
+  # Initialize category.
+  if !current_account[:categories][category]
+    current_account[:categories][category] = {
+      tally: 0.00,
+      num_transactions: 0,
+      average_transaction_cost: 0.00
+    }
   end
 
-  def selectAccount name
-    data = csvData
-    Account.new(name, data[name])
-  end
+  # Tally category.
+  current_account[:categories][category][:tally] += transaction_amount
 
-  def readLoginFile
-    accounts = []
-    File.open('logins.txt', 'r').each do |line|
-      accounts << line.chomp.split(', ')
-    end
-    accounts
-  end
+  # Increment transaction counter.
+  current_account[:categories][category][:num_transactions] += 1
 
-  def checkLoginInfo
-    accounts = readLoginFile
-    return accounts.include? [params['username'],params['password']]
-  end
-
-  def logIn(username,password)
-    session[:user] = username
-  end
-
-  def LoggedIn
-    if session[:user] != nil
-      LoggedIn = true
-  end
-
+  # Update average transaction cost.
+  current_account[:categories][category][:average_transaction_cost] = current_account[:categories][category][:tally] / current_account[:categories][category][:num_transactions] 
+end
 end
